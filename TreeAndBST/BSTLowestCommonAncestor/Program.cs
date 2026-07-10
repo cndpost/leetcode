aa913a22dc561995bcfc9255c6d7ea9dfead8f15
cndpost <cndpost@yahoo.com>
2026-07-09 20:51:01 -0700

    added TreesBST

---
+++ TreeAndBST/BSTLowestCommonAncestor/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+// trick: recursion on the left or right. Then root is the answer.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+                 6
+               /   \
+              2     8
+             / \   / \
+            0   4 7   9
+               / \
+              3   5
+        */
+
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(8);
+
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+
+        TreeNode p = root.left;        // 2
+        TreeNode q = root.left.right;  // 4
+
+        TreeNode lca = LowestCommonAncestor(root, p, q);
+
+        Console.WriteLine($"LCA of {p.val} and {q.val} = {lca.val}");
+    }
+
+    public static TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        while (root != null)
+        {
+            if (p.val < root.val && q.val < root.val)
+            {
+                root = root.left;
+            }
+            else if (p.val > root.val && q.val > root.val)
+            {
+                root = root.right;
+            }
+            else
+            {
+                return root;
+            }
+        }
+
+        return null;
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int v)
+    {
+        val = v;
+    }
+}
---
+++ TreeAndBST/TreeSerializerDeserializer/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+// trick: deserialize using pre-order traversal: root, left, right
+// serilize also use pre-order traversal: root, left, right
+//
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+                 1
+               /   \
+              2     3
+                   / \
+                  4   5
+        */
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(5);
+
+        string serialized = Serialize(root);
+        Console.WriteLine("Serialized Tree:");
+        Console.WriteLine(serialized);
+
+        TreeNode restored = Deserialize(serialized);
+        Console.WriteLine("\nDeserialized Tree (Preorder Print):");
+        PrintPreorder(restored);
+    }
+
+    public static string Serialize(TreeNode root)
+    {
+        List<string> list = new List<string>();
+        SerializeHelper(root, list);
+        return string.Join(",", list);
+    }
+
+    private static void SerializeHelper(TreeNode node, List<string> list)
+    {
+        if (node == null)
+        {
+            list.Add("null");
+            return;
+        }
+
+        list.Add(node.val.ToString());
+        SerializeHelper(node.left, list);
+        SerializeHelper(node.right, list);
+    }
+
+    public static TreeNode Deserialize(string data)
+    {
+        Queue<string> tokens = new Queue<string>(data.Split(','));
+        return DeserializeHelper(tokens);
+    }
+
+    private static TreeNode DeserializeHelper(Queue<string> tokens)
+    {
+        string val = tokens.Dequeue();
+
+        if (val == "null")
+            return null;
+
+        TreeNode node = new TreeNode(int.Parse(val));
+        node.left = DeserializeHelper(tokens);
+        node.right = DeserializeHelper(tokens);
+
+        return node;
+    }
+
+    public static void PrintPreorder(TreeNode node)
+    {
+        if (node == null)
+        {
+            Console.Write("null ");
+            return;
+        }
+
+        Console.Write(node.val + " ");
+        PrintPreorder(node.left);
+        PrintPreorder(node.right);
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int v)
+    {
+        val = v;
+    }
+}
